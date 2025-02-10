0ed5266ecb0455262d2fa11373fd65571551931c
Dom <dowipf01@wsc.edu>
2025-02-10 09:30:40 -0600

    getters and setters and deposit

--- src/Account.java
+++ src/Account.java
@@ -1,6 +1,30 @@
-public class Account {
-    public static void main(String[] args) {
+/**
+ * The Account class. Used to create the Account class.
+ *
+ * @author Brock Koranda Dom Wipf
+ * @version beta 0.1
+ * @since 2025.02.10
+ */
 
-        System.out.println("Hello world!");
+public class Account {
+    String firstName = "?";
+    String LastName = "?";
+    double balance = 0;
+    public Account(String firstName,String lastName) {
+        this.firstName = firstName;
+        this.LastName = lastName;
+        this.balance = 0;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
+    public void deposit(double amount) {
+        this.balance += amount;
     }
 }
